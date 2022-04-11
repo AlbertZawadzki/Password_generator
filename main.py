ea760249791a0041d71cb32490146dcd962b00ca
MyWillIsMyOwn <weyerfilip@gmail.com>
2022-04-11 20:14:32 +0200

    good

---
+++ main.py
@@ -0,0 +1,26 @@
+import random
+import string
+
+def password():
+    while True:
+        strength = input("How stron password do you want? w/m/s")
+        match strength:
+            case "w":
+                return ''.join(random.choice(string.ascii_letters) for _ in range(length))
+                break
+            case "m":
+                return ''.join(random.choice(string.ascii_letters + string.ascii_letters) for _ in range(length))
+                break
+            case "s":
+                return ''.join(random.choice(string.ascii_letters + string.ascii_letters + string.punctuation) for _ in range(length))
+                break
+
+question = input("Do you want to generate password? y/n")
+
+if question=="y":
+    while True:
+        length = int(input("How long password do you want to receive?"))
+        print(password())
+        answer = input("Is this password satisfied? y/n")
+        if answer=="y":
+            break
